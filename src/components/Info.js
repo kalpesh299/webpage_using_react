a46f2f225f8563896a812e220553b698604721e1
Kalpesh Patil <72344646+kalpesh299@users.noreply.github.com>
2023-11-17 17:15:09 +0530

    info heading change

--- src/components/Info.js
+++ src/components/Info.js
@@ -5,7 +5,7 @@ export const Info = () => {
     <div>
         
 
-        <p class="mb-3 mt-3 font-bold  text-lg text-teal-400 md:text-xl dark:text-gray-400">Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work and deploy.</p>
+        <p class="mb-3 mt-3 font-bold  text-lg text-teal-400 md:text-xl dark:text-gray-400">Deliver great service experiences fast - without the complexity IN our Organization.</p>
 <p class="text-gray-500  dark:text-gray-400">A hospital contains one or more wards that house hospital beds for inpatients. It may also have acute services such as an emergency department, operating theatre hospital may be classified as a trauma center.</p>
 <p class="text-gray-500  dark:text-gray-400">A hospital contains one or more wards that house hospital beds for inpatients. It may also have acute services such as an emergency department, operating theatre hospital may be classified as a trauma center.</p>
 <p class="text-gray-500  dark:text-gray-400">A hospital contains one or more wards that house hospital beds for inpatients. It may also have acute services such as an emergency department, operating theatre hospital may be classified as a trauma center.</p>
