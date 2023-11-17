8e3626ff029ac27559dfe814f08668a50afcaaa3
Kalpesh Patil <72344646+kalpesh299@users.noreply.github.com>
2023-11-17 17:05:01 +0530

    2nd Cpmmit

---
+++ src/components/Image.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Image = () => {
+  return (
+    <div className="w-full   h-96">
+        <img className="w-full h-full object-fit" src='https://img.freepik.com/free-photo/young-handsome-physician-medical-robe-with-stethoscope_1303-17818.jpg'></img>
+    </div>
+  )
+}
---
+++ src/components/Info.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Info = () => {
+  return (
+    <div>
+        
+
+        <p class="mb-3 mt-3 font-bold  text-lg text-teal-400 md:text-xl dark:text-gray-400">Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work and deploy.</p>
+<p class="text-gray-500  dark:text-gray-400">A hospital contains one or more wards that house hospital beds for inpatients. It may also have acute services such as an emergency department, operating theatre hospital may be classified as a trauma center.</p>
+<p class="text-gray-500  dark:text-gray-400">A hospital contains one or more wards that house hospital beds for inpatients. It may also have acute services such as an emergency department, operating theatre hospital may be classified as a trauma center.</p>
+<p class="text-gray-500  dark:text-gray-400">A hospital contains one or more wards that house hospital beds for inpatients. It may also have acute services such as an emergency department, operating theatre hospital may be classified as a trauma center.</p>
+<p class="text-gray-500 mb-4 dark:text-gray-400">A hospital contains one or more wards that house hospital beds for inpatients. It may also have acute services such as an emergency department, operating theatre hospital may be classified as a trauma center.</p>
+   <p class="text-gray-500  dark:text-gray-400">A hospital contains one or more wards that house hospital beds for inpatients. It may also have acute services such as an emergency department, operating theatre hospital may be classified as a trauma center.</p>
+<p class="text-gray-500  dark:text-gray-400">A hospital contains one or more wards that house hospital beds for inpatients. It may also have acute services such as an emergency department, operating theatre hospital may be classified as a trauma center.</p>
+<p class="text-gray-500  dark:text-gray-400">A hospital contains one or more wards that house hospital beds for inpatients. It may also have acute services such as an emergency department, operating theatre hospital may be classified as a trauma center.</p>
+<p class="text-gray-500 mb-4 dark:text-gray-400">A hospital contains one or more wards that house hospital beds for inpatients. It may also have acute services such as an emergency department, operating theatre hospital may be classified as a trauma center.</p>
+   
+    </div>
+  )
+}
